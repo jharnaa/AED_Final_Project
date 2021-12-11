47ae1f6606a736c09b656557fa2334aeeeac7210
Jharna Doda <doda.j@northeastern.edu>
2021-12-11 17:14:57 -0500

    added personalised options

--- AED_final/src/Business/customer/PersonalisedOrderItem.java
+++ AED_final/src/Business/customer/PersonalisedOrderItem.java
@@ -10,25 +10,29 @@ package Business.customer;
  * @author jharnadoda
  */
 public class PersonalisedOrderItem {
-        private float Price;
+    
     private CustomerApparel personaliseProduct;
     private int orderItemId;
+    private int customizeProductId;
+    private String category;
+    private String style;
+    private String color;
+    private String size;
+    private String fabricType;
+    private String imageLocation;
+    private String text;
+    private String textLocation;
+    private String designTechnique;
+    private float price;
     private static int count = 0;
+    
 
     public PersonalisedOrderItem() {
         personaliseProduct = new CustomerApparel();
         count++;
         orderItemId = count;
     }
-
-    public float getPrice() {
-        return Price;
-    }
-
-    public void setPrice(float Price) {
-        this.Price = Price;
-    }
-
+    
     public CustomerApparel getPersonaliseProduct() {
         return personaliseProduct;
     }
@@ -36,9 +40,110 @@ public class PersonalisedOrderItem {
     public void setPersonaliseProduct(CustomerApparel personaliseProduct) {
         this.personaliseProduct = personaliseProduct;
     }
+    
+
+    public int getOrderItemId() {
+        return orderItemId;
+    }
 
     public void setOrderItemId(int orderItemId) {
         this.orderItemId = orderItemId;
+    }
+
+    public int getCustomizeProductId() {
+        return customizeProductId;
+    }
+
+    public void setCustomizeProductId(int customizeProductId) {
+        this.customizeProductId = customizeProductId;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getFabricType() {
+        return fabricType;
+    }
+
+    public void setFabricType(String fabricType) {
+        this.fabricType = fabricType;
+    }
+
+    public String getImageLocation() {
+        return imageLocation;
+    }
+
+    public void setImageLocation(String imageLocation) {
+        this.imageLocation = imageLocation;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getTextLocation() {
+        return textLocation;
+    }
+
+    public void setTextLocation(String textLocation) {
+        this.textLocation = textLocation;
+    }
+
+    public String getDesignTechnique() {
+        return designTechnique;
+    }
+
+    public void setDesignTechnique(String designTechnique) {
+        this.designTechnique = designTechnique;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void setCount(int count) {
+        PersonalisedOrderItem.count = count;
     }
 
     
