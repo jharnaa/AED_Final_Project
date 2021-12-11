863699d8c068783c6bc7716546ac61cf9ea1d76f
Mrunal Bhalerao <bhalerao.m@northeastern.edu>
2021-12-11 11:01:00 -0500

    MainJFrame

--- AED_final/src/Business/UserAccount/UserAccount.java
+++ AED_final/src/Business/UserAccount/UserAccount.java
@@ -17,8 +17,25 @@ public class UserAccount {
 
     private String username;
     private String password;
+    private String password1;
+
+    public String getPassword1() {
+        return password1;
+    }
+
+    public void setPassword1(String password1) {
+        this.password1 = password1;
+    }
     private Person employee;
     private Role role;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
     private WorkQueue workQueue;
 
     public UserAccount() {
@@ -31,14 +48,6 @@ public class UserAccount {
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public Role getRole() {
